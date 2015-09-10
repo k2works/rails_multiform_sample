24257ba584613b4274066dee4c6bb56539774567
k2works <kakimomokuri@gmail.com>
2015-09-10 16:17:53 +0900

    Addressモデルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910070826) do
+ActiveRecord::Schema.define(version: 20150910071622) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "kind"
+    t.string   "street"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
