9a509ff5d914f56ac6dd2e9292bfd6f263c1f1e7
k2works <kakimomokuri@gmail.com>
2015-09-10 16:55:49 +0900

    Personモデルを編集する

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-  accepts_nested_attributes_for :addresses, allow_destroy: true
+  accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: lambda {|attributes| attributes['kind'].blank?}
 end
